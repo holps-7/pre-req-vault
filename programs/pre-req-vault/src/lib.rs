4617df797b669375113bf1201df644728c903538
Ramesh Sachan <43104299+holps-7@users.noreply.github.com>
2026-08-20 18:22:42 +0530

    update pre_req_vault ID and implement initialization in withdraw function

--- programs/pre-req-vault/src/lib.rs
+++ programs/pre-req-vault/src/lib.rs
@@ -9,7 +9,7 @@ pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("DBoobRVqT7PaAhq8obqo95723CHyWvcCLRUKvQ5wYWE4");
+declare_id!("Seq2xKSNAo9iwoToBvu5oj174UqHUTpejGFzaDq1uR5");
 
 #[program]
 pub mod pre_req_vault {
